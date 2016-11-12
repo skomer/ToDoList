c040ad22f6d84f1ec919b4b68de0366e0ed7a3b4
Josephine Elder <parusmontanus@gmail.com>
2016-11-12 11:32:30 +0000

    Created ToDoItem class and tests

--- app/src/main/java/com/example/user/todolist/ToDoItem.java
+++ app/src/main/java/com/example/user/todolist/ToDoItem.java
@@ -1,8 +1,61 @@
 package com.example.user.todolist;
 
-/**
- * Created by user on 12/11/2016.
- */
+import java.util.Date;
+
 
 public class ToDoItem {
+
+    public String title;
+    public String description;
+    public Date whenCompleted;
+    public int categoryIndex;
+
+    public static final String[] categories = {"low", "medium", "high"};
+
+    public ToDoItem(String title, String description, int categoryIndex) {
+        this.title = title;
+        this.description = description;
+        this.categoryIndex = categoryIndex;
+        this.whenCompleted = null;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String newDescription) {
+        this.description = newDescription;
+    }
+
+    public String getCategory() {
+        return categories[this.categoryIndex];
+    }
+
+    public void setCategory(int newCategoryIndex) {
+        this.categoryIndex = newCategoryIndex;
+    }
+
+    public Date getWhenCompleted() {
+        return this.whenCompleted;
+    }
+
+    public void setWhenCompleted() {
+        Date dateNow = new Date();
+        this.whenCompleted = dateNow;
+    }
+
+
+
+
+
+
+
 }
--- app/src/test/java/com/example/user/todolist/ToDoItemTest.java
+++ app/src/test/java/com/example/user/todolist/ToDoItemTest.java
@@ -1,8 +1,55 @@
 package com.example.user.todolist;
 
-/**
- * Created by user on 12/11/2016.
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 
 public class ToDoItemTest {
+
+    ToDoItem getAJob;
+    ToDoItem cleanFlat;
+    ToDoItem makeCushion;
+
+    @Before
+    public void before() {
+        getAJob = new ToDoItem("Get a job", "Figure out what kind of job I would like and tailor CV.", 2);
+        cleanFlat = new ToDoItem("Clean the flat", "Wash the floors because they are sticky", 1);
+        makeCushion = new ToDoItem("Make cushion", "Keep piecing the shapes and think about the layout", 0);
+    }
+
+    @Test
+    public void testItemHasTitle() {
+        assertEquals("Clean the flat", cleanFlat.getTitle());
+    }
+
+    @Test
+    public void testCanSetItemTitle() {
+        cleanFlat.setTitle("Clean my flat");
+        assertEquals("Clean my flat", cleanFlat.getTitle());
+    }
+
+    @Test
+    public void testItemHasDescription() {
+        assertEquals("Wash the floors because they are sticky", cleanFlat.getDescription());
+    }
+
+    @Test
+    public void testCanSetItemDescription() {
+        makeCushion.setDescription("Make hexagons");
+        assertEquals("Make hexagons", makeCushion.getDescription());
+    }
+
+    @Test
+    public void testItemHasCategory() {
+        assertEquals("high", getAJob.getCategory());
+    }
+
+    @Test
+    public void testCanSetCategory() {
+        cleanFlat.setCategory(0);
+        assertEquals("low", cleanFlat.getCategory());
+    }
+
 }
